2dcebebce67bd8c0dfe1518345087e53f33c614f
yingrong <yingrong.hu@gmail.com>
2013-01-01 11:26:05 +0800

    增加 hello git
    Signed-off-by: yingrong <yingrong.hu@gmail.com>

--- algorighms/src/yingrong/hello/HelloAlgorighms.java
+++ algorighms/src/yingrong/hello/HelloAlgorighms.java
@@ -3,5 +3,6 @@ package yingrong.hello;
 public class HelloAlgorighms {
 	public static void main(String[] args) {
 		System.out.println("hello algorighms!");
+		System.out.println("hello git");
 	}
 }
