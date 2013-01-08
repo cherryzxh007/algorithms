a0dcb1c4f90f5951ed88b546cfcfc85679fabfe0
yingrong <yingrong.hu@gmail.com>
2013-01-08 23:24:46 +0800

    增加Stack，TODO Queue
    Signed-off-by: yingrong <yingrong.hu@gmail.com>

--- algorighms/src/yingrong/fundamentals/ex03/Queue.java
+++ algorighms/src/yingrong/fundamentals/ex03/Queue.java
@@ -1,14 +1,51 @@
 package yingrong.fundamentals.ex03;
 
-public abstract class Queue<Item> implements Iterable<Item> {
+import java.util.Iterator;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class Queue<Item> implements Iterable<Item> {
 	public Queue() {
-	};
+	}
 
-	public abstract void enqueue(Item item);
+	public void enqueue(Item item) {
+	}
 
-	public abstract void dequeue(Item item);
+	public String dequeue() {
+		return null;
+	}
 
-	public abstract boolean isEmpty();
+	public boolean isEmpty() {
+		return false;
+	}
 
-	public abstract int size();
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Queue<String> q = new Queue<String>();
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-"))
+				q.enqueue(item);
+			else if (!q.isEmpty())
+				StdOut.print(q.dequeue() + "");
+		}
+		StdOut.println("(" + q.size() + " left on queue)");
+	}
+
 }
--- algorighms/src/yingrong/fundamentals/ex03/Stack.java
+++ algorighms/src/yingrong/fundamentals/ex03/Stack.java
@@ -1,35 +1,147 @@
 package yingrong.fundamentals.ex03;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Stack<Item> implements Iterable<Item>{
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class Stack<Item> implements Iterable<Item> {
+
+	private int N;
+	private Node first;
+
+	private class Node {
+		private Item item;
+		private Node next;
+	}
+
+	/**
+	 * 创建一个空栈 长度为0，并且first指向null。
+	 */
 	public Stack() {
+		first = null;
+		N = 0;
 	}
 
-	public   void push(Item item){
-		
+	public boolean isEmpty() {
+		return N == 0;
+	};
+
+	public int size() {
+		return N;
 	}
 
-	public   Item pop(Item item){
-		return null;
+	public void push(Item item) {
+		Node oldFirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldFirst;
+		N++;
+		assert check();
+	}
+
+	public Item pop() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Stack underflow");
+		}
+		Item item = first.item;
+		first = first.next;
+		N--;
+		assert check();
+		return item;
 	};
 
-	public   boolean isEmpty(){
-		return false;
-	};
+	private boolean check() {
+		if (N == 0) {
+			if (first != null) {
+				return false;
+			}
+		} else if (N == 1) {
+			if (first == null) {
+				return false;
+			}
+			if (first.next == null) {
+				return false;
+			}
+		} else {
+			if (first.next == null) {
+				return false;
+			}
+		}
 
-	public   int size(){
-		return 0;
+		int numberOfNodes = 0;
+		for (Node x = first; x != null; x = x.next) {
+			numberOfNodes++;
+		}
+		if (numberOfNodes != N) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public Item peek() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Stack underflow");
+		}
+		return first.item;
+	}
+
+	@Override
+	public String toString() {
+		return toString(" ");
+	}
+
+	public String toString(String split) {
+		StringBuffer buffer = new StringBuffer();
+		for (Item item : this) {
+			buffer.append(item + split);
+		}
+		return buffer.toString();
 	}
 
 	@Override
 	public Iterator<Item> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ListIterator();
 	};
-	
-	public static void main(String[] args) {
-		
+
+	private class ListIterator implements Iterator<Item> {
+		private Node current = first;
+
+		@Override
+		public boolean hasNext() {
+			return current.next != null;
+		}
+
+		@Override
+		public Item next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
 	}
-	
+
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<String>();
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-")) {
+				stack.push(item);
+			} else if (!stack.isEmpty()) {
+				StdOut.print(stack.pop() + " ");
+			}
+		}
+		StdOut.println("(" + stack.size() + " left on stack)");
+	}
+
 }
